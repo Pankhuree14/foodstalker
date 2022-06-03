8be255452b5ac41dd36170a6f86685a60ed7622e
Pankhuree14 <82907405+Pankhuree14@users.noreply.github.com>
2022-06-03 16:07:04 +0530

    Delete Frontend directory

--- Frontend/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-        return (
-            <p style={{backgroundColor:"#FDC6E8",textAlign:"center", marginBottom:"25px"}}>©FOOSTALKER.COM PRIVACY POLICY TERMS </p>
-        );
-};
